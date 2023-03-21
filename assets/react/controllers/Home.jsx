615ced5506b213186e353386de01fa24b2c0dda6
Robin Payet <robinpayet@gmail.com>
2023-03-21 17:24:40 +0100

    Demo Search

--- assets/react/controllers/Home.jsx
+++ assets/react/controllers/Home.jsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Home({ companies }) {
+
+    let [search, setSearch] = useState('');
+
+    
     return (
     <div>
 
         <h2 className="text-blue-500 font-bold text-2xl">
             Hello React!
         </h2>
-
+        
+        <input value={search} onChange={e => setSearch(e.target.value)} />
         <ul>
-            {companies.map(company => <li>{company}</li>)}
+            {companies.filter(company => company.toLowerCase().includes(search.toLowerCase())).map(company => <li>{company}</li>)}
         </ul>
     </div>
     )
