03e7c0c407919e7e820b23e84e118a062dc04595
Rpayet <robinpayet@gmail.com>
2023-03-23 09:07:23 +0100

    New Review Setup

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,7 +5,9 @@ namespace App\Controller;
 use App\Entity\Review;
 use App\Form\ReviewType;
 use App\Repository\ReviewRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request as HttpFoundationRequest;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -17,17 +19,31 @@ class HomeController extends AbstractController
     {
         return $this->render('home/index.html.twig', [
             'companies' => ['Apple', 'Boxydev', 'Microsoft'],
-            'reviews' => $repository->findAll(),
+            'reviews' => $repository->findby([], ['createdAt' => 'DESC']),
         ]);
     }
 
     #[Route('/review/new', name: 'app_review_new')]
     #[IsGranted('ROLE_USER')]
-    public function new()
+    public function new(HttpFoundationRequest $request, EntityManagerInterface $manager, ReviewRepository $repository)
     {
 
         $review = New Review();
         $form = $this->createForm(ReviewType::class, $review);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $review->setCreatedAt(new \DateTimeImmutable());
+            $review->setUser($this->getUser());
+
+            $manager->persist($review);
+            $manager->flush();
+
+            // $repository->save($review, true);
+
+            return $this->redirectToRoute('app_home');
+        }
         
         return $this->render('review/new.html.twig', [
             'form' => $form,
